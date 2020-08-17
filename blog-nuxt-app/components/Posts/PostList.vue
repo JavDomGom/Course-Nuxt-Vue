c1e89099846231da9246299dc6027ad48f359597
JavierDominguezGomez <jdomgo3@gmail.com>
2020-08-17 18:51:46 +0200

    Página Posts.

---
+++ blog-nuxt-app/components/Posts/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbRgucQ5ANNoAXfK8VZgiNb46V9C0D8xL-UA&usqp=CAU"
+      title="Hello there!"
+      previewText="This is my first post!"
+    />
+    <PostPreview
+      id="2"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbRgucQ5ANNoAXfK8VZgiNb46V9C0D8xL-UA&usqp=CAU"
+      title="Hello there - the second time!"
+      previewText="This is my second post!"
+    />
+    <PostPreview
+      id="3"
+      thumbnail="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRbRgucQ5ANNoAXfK8VZgiNb46V9C0D8xL-UA&usqp=CAU"
+      title="Hi!"
+      previewText="This is my third post!"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+
+export default {
+  components: {
+    PostPreview,
+  },
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
